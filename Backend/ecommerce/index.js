174413632ce001db34300f185a4bad10952bf2e0
vignesh6664 <vignesh.dev.main@gmail.com>
2024-07-02 11:57:07 +0530

    Reduced number of lines in source code

--- Backend/ecommerce/index.js
+++ Backend/ecommerce/index.js
@@ -3,13 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
-// const productRoutes = require("./routes/product");
 const newProductRoutes = require("./routes/newProduct");
 const pantRoutes = require("./routes/pants");
 const sweatShirtRoutes = require("./routes/sweatShirt");
-const shirtRoutes = require("./routes/shirt");
-const pantssecRoutes = require("./routes/pantssec");
-const tshirtRoutes = require("./routes/tshirts");
 const productRoutes = require("./routes/Products");
 
 const app = express();
@@ -20,9 +16,6 @@ app.use("/", authRoutes);
 app.use("/", newProductRoutes);
 app.use("/", pantRoutes);
 app.use("/", sweatShirtRoutes);
-app.use("/", shirtRoutes);
-app.use("/", pantssecRoutes);
-app.use("/", tshirtRoutes);
 app.use("/", productRoutes);
 
 const PORT = process.env.PORT || 5000;
